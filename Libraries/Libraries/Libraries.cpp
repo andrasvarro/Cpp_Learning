ee57625d795d0cfe40724e623cb9df21af2d14e6
AD001/Z001X91H <andras.varro@siemens.com>
2018-07-14 17:10:17 -0400

    Learning Libraries

---
+++ Libraries/Libraries/Libraries.cpp
@@ -0,0 +1,11 @@
+// Libraries.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
---
+++ Libraries2/Libraries2/Source.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << 3.0/2 << std::endl << "Hello" +3 << std::endl;
+    return 0;
+}
