2df301ab28be6749b30d422a31a0912603921b07
Andras Varro <andras.varro@siemens.com>
2018-07-16 18:16:00 -0400

    member functions

--- StringsAndCollections/StringsAndCollections/Source.cpp
+++ StringsAndCollections/StringsAndCollections/Source.cpp
@@ -14,5 +14,15 @@ int main()
     }
     
     cout << greeting << endl;
+
+    int l = greeting.length();
+    cout << "\"" + greeting + "\" is " << l << " character long." << endl;
+    auto space = greeting.find(' ');
+    string beginning = greeting.substr(space + 1);
+    cout << beginning << endl;
+    if (beginning == name)
+    {
+        cout << "expected result." << endl;
+    }
     return 0;
 }
