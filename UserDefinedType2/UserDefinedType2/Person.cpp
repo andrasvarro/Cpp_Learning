295c1317c9caa0747ab9de6d626273eb766c9751
Andras Varro <andras.varro@siemens.com>
2018-07-23 17:04:46 -0400

    simplifying namespaces

--- UserDefinedType2/UserDefinedType2/Person.cpp
+++ UserDefinedType2/UserDefinedType2/Person.cpp
@@ -1,24 +1,28 @@
 #include "Person.h"
 #include <iostream>
 
-Person::Person(std::string first, std::string last, int arbitrary): firstname(first), lastname(last), arbitrarynumber(arbitrary)
+using std::cout;
+using std::endl;
+using std::string;
+
+Person::Person(string first, string last, int arbitrary): firstname(first), lastname(last), arbitrarynumber(arbitrary)
 {
-    std::cout << "constructing " << getName() << std::endl;
+    cout << "constructing " << getName() << endl;
 }
 
 Person::Person() : arbitrarynumber(0)
 {
-    std::cout << "constructing " << firstname << " " << lastname << std::endl;
+    cout << "constructing " << firstname << " " << lastname << endl;
 
 }
 
 Person::~Person()
 {
-    std::cout << "destructing " << firstname << " " << lastname << std::endl;
+    cout << "destructing " << firstname << " " << lastname << endl;
 
 }
 
-std::string Person::getName()
+string Person::getName()
 {
-	return firstname + " " + lastname;
+    return firstname + " " + lastname;
 }
